bc9e4ce6795d49146f9e3d2d8ca424ed5b5ac5c7
Benjamín Alejandro Namor Castro <181518793+B-Namor@users.noreply.github.com>
2025-01-31 19:41:57 -0300

    ¿Qué es eso de la eficiencia?

--- rawGlitcherDetector.py
+++ rawGlitcherDetector.py
@@ -1,3 +1,5 @@
+import sympy as sp
+
 def readFile(path):
     """
     Reads a given file and split it's content into three new lists: frequencies, time of arrival and errors
@@ -26,3 +28,23 @@ def readFile(path):
 
 path = input("Ingrese el paso de su archivo (con formato): ")
 freq, toa, err = readFile(path)
+
+# Intervals are calculated
+intervalNumber = int(input("Ingrese la cantidad de intervalos deseados para realizar sub-intervalos de\nfrecuencias (ingrese un número entero positivo): "))
+toaRange = toa[-1] - toa[0]
+intervalLenght = toaRange/intervalNumber
+# Limits of each interval are determinated
+intervals = []
+for i in range(1,intervalNumber):
+    intervals.append(toa[0]+intervalLenght*i)
+intervals.append(toa[-1])
+
+# Lists to add each time to it's corresponding interval is created
+toaSubIntervals = {}
+for i in range(intervalNumber):
+    toaSubIntervals[f'f0_{i}'] = []
+# Each time is added to it's corresponding list
+for i in range(intervals):
+    for elem in toa:
+        if elem < intervals[i]:
+            toaSubIntervals[f'f0_{i}'].append(elem)
